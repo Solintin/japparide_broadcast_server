78a6a6178e88c8cea2aba713427836b54a822f50
DevSaalih <soliualley@gmail.com>
2022-10-23 23:34:29 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const io = require("socket.io")(3000, {
+  cors: {
+    origin: ["http://localhost:8080", "http://192.168.182.147:8080", "https://japparide.netlify.app"],
+  },
+});
+
+// var clients = io.sockets.clients();
+
+// console.log(clients);
+// const sockets = (await io.fetchSockets()).map(socket => socket.id);
+// console.log(sockets);
+io.on("connect", (socket) => {
+  console.log(socket.id); 
+  socket.on('send-request', (request) =>{
+    io.emit('get-request', request)
+    console.log(request);
+  })
+  socket.on('send-accept', (message, passenger) =>{
+    io.to(passenger).emit('get-accept', message)
+    console.log(message);
+  })
+
+});
+
+
