e80200ca4d063186ac902d5d5a4be1c5e759c14f
DevSaalih <soliualley@gmail.com>
2022-12-03 14:28:04 +0100

    update

--- server.js
+++ server.js
@@ -7,18 +7,54 @@ const io = require("socket.io")(server, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
-  }
+  },
 });
 
 const cors = require("cors");
 app.use(cors());
 
+let users = [];
+let drivers = [];
+const addUsers = (newUser) => {
+  const isExist = users.some((x) => x.userId === newUser.userId);
+  console.log(isExist);
+  if (!isExist) {
+    users.push(newUser);
+  }
+  addDrivers();
+  console.log(users);
+};
+const addDrivers = () => {
+  const getDrivers = users.filter((x) => x.userType === "driver");
+  // console.log(getDrivers);
+  drivers = getDrivers;
+};
+
 io.on("connect", (socket) => {
-  console.log(socket.id);
+  console.log(socket.id + " connected");
   socket.on("send-request", (request) => {
-    io.emit("get-request", request);
-    console.log(request);
+    //request.user = passenger
+    console.log("All drivers ", drivers);
+    io.to(request.user).emit("get-drivers", drivers);
+    console.log("Request sent ", request);
+
+    // io.emit("get-request", request);
+    if (drivers.length > 0) {
+      io.to(drivers[1].socket_id).emit("get-request", request);
+    }
   });
+  socket.on("send-user-info", (userInfo) => {
+    const userData = {
+      ...userInfo,
+      socket_id: socket.id,
+    };
+    console.log(userData);
+    addUsers(userData);
+  });
+  // socket.on("get-drivers", (passenger) => {
+  //   io.to(passenger).emit("get-drivers", drivers);
+  // });
+
   socket.on("send-accept", (message, passenger, driver) => {
     io.to(passenger).emit("get-accept", driver);
     console.log(message);
@@ -28,8 +64,16 @@ io.on("connect", (socket) => {
     io.to(driver).emit("get-passenger-cancel-request");
     console.log("Request Cancel from passenger");
   });
-  socket.on("driver-cancel-request", (user) => {
-    io.to(user).emit("get-driver-cancel-request");
+  socket.on("driver-cancel-request", (user, request) => {
+    if (drivers.length > 0) {
+      console.log("re-routing");
+      console.log("re-routing-data ", request);
+      io.to(drivers[Math.floor(Math.random() * 2)].socket_id).emit(
+        "get-request",
+        request
+      );
+    }
+    // io.to(user).emit("get-driver-cancel-request");
     console.log("Request Cancel from driver");
   });
   socket.on("ride-completed", (user) => {
@@ -41,6 +85,14 @@ io.on("connect", (socket) => {
     console.log(user);
     io.to(user).emit("get-start-ride");
     console.log("Ride started");
+  });
+
+  socket.on("disconnect", () => {
+    console.log(socket.id + " disconnected");
+    const allUsers = users.filter((user) => user.socket_id !== socket.id);
+    users = allUsers;
+    addDrivers();
+    console.log(users);
   });
 });
 
