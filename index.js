ac6a36ab0e90ce8cfd988e0488b4c1d21d036036
DevSaalih <soliualley@gmail.com>
2022-12-04 15:40:36 +0100

    test

--- index.js
+++ index.js
@@ -19,16 +19,16 @@ const server = express()
   .use((req, res) => res.send("Hello Websocket"))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const io = socketIO(server,  {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST", "OPTIONS", "PUT", "PATCH", "DELETE"],  
-  },
-});
+// const io = socketIO(server,  {
+//   cors: {
+//     origin: "*",
+//     methods: ["GET", "POST", "OPTIONS", "PUT", "PATCH", "DELETE"],  
+//   },
+// });
 
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
-});
+// io.on('connection', (socket) => {
+//   console.log('Client connected');
+//   socket.on('disconnect', () => console.log('Client disconnected'));
+// });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+// setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
