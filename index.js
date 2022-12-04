134bb9fce647dd246c502e8f25adc3d952b0d9d4
DevSaalih <soliualley@gmail.com>
2022-12-04 09:54:49 +0100

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .use((req, res) => res.sendFile("Hello Websocket"))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server,  {
